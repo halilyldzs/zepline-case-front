0ee0c4dbe0a5263981f86759c4453ab31fe73059
halil <halilyldzs@gmail.com>
2026-01-17 21:53:16 +0300

    Update JournalIsuesList and JournalIssueCard components for improved layout and responsiveness; adjust column widths and enhance styling for better presentation of journal issues.

--- app/home/_components/JournalIssues/JournalIsuesList/JournalIssueCard/JournalIssueCard.tsx
+++ app/home/_components/JournalIssues/JournalIsuesList/JournalIssueCard/JournalIssueCard.tsx
@@ -1,27 +1,28 @@
 import Image from 'next/image';
 import Badge from '@/components/ui/Badge';
-import Button from '@/components/ui/Button';
-import { JournalIssueCardProps } from './JournalIssueCard.types';
 import './JournalIssueCard.scss';
+import Icon from '@/components/ui/Icon';
 
-export default function JournalIssueCard({
-  image = '/images/journal-image.png',
-  badge = 'Sayı 1',
-  title = 'Profuture Teknoloji Dergisi',
-  description = 'Dergi içeriği ve makaleler hakkında detaylı bilgiler...',
-  date = '2024',
-  issueNumber = 'Vol. 1',
-  className,
-}: JournalIssueCardProps) {
+export default function JournalIssueCard() {
   return (
-    <article className={`journal-issue-card ${className || ''}`}>
-      <Image
-        src={image}
-        alt={title}
-        width={300}
-        height={400}
-        className="journal-issue-card__image"
-      />
+    <article className="journal-issue-card">
+      <div className="journal-issue-card__wrapper">
+        <Image
+          src="/images/journal-image.png"
+          alt="Journal Image"
+          width={191.32}
+          height={285}
+          priority
+          className="journal-issue-card__image"
+        />
+
+        <div className="d-flex flex-column gap-2 w-100">
+          <div className="d-flex align-items-center justify-content-between">
+            <Icon name="pdf" className="journal-issue-card__icon" />
+            <Badge variant="info">Yeni</Badge>
+          </div>
+        </div>
+      </div>
     </article>
   );
 }
--- app/home/_components/JournalIssues/JournalIsuesList/JournalIsuesList.tsx
+++ app/home/_components/JournalIssues/JournalIsuesList/JournalIsuesList.tsx
@@ -4,13 +4,13 @@ import './JournalIsuesList.scss';
 export default function JournalIsuesList() {
   return (
     <section className="row journal-issues-list">
-      <div className="col-12 col-md-6 col-lg-3">
+      <div className="col-12 col-md-6 col-lg-4">
         <JournalIssueCard />
       </div>
-      <div className="col-12 col-md-6 col-lg-3">
+      <div className="col-12 col-md-6 col-lg-4">
         <JournalIssueCard />
       </div>
-      <div className="col-12 col-md-6 col-lg-3">
+      <div className="col-12 col-md-6 col-lg-4">
         <JournalIssueCard />
       </div>
     </section>
