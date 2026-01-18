1f1b5051359aa23af7890a4ae6ab742f63545ffa
halil <halilyldzs@gmail.com>
2026-01-18 20:39:06 +0300

    Refactor styles in SendArticle and LatestArticlesCard components for improved responsiveness: adjust padding, margins, and layout properties, and update media queries to enhance adaptability across various screen sizes.

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__background">
-        <div className="footer__content">
+        <div className="footer__content container">
           <FooterContact />
           <FooterLegal />
         </div>
