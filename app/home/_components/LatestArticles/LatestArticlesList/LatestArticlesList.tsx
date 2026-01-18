4015e6a9e0183ed75875d7ce1fc13b1eaccbe662
halil <halilyldzs@gmail.com>
2026-01-18 14:48:48 +0300

    Add LatestArticlesList component with header, title, and subtitle; enhance SCSS for layout and styling improvements.

--- app/home/_components/LatestArticles/LatestArticlesList/LatestArticlesList.tsx
+++ app/home/_components/LatestArticles/LatestArticlesList/LatestArticlesList.tsx
@@ -1,5 +1,18 @@
+import Button from '@/components/ui/Button';
 import './LatestArticlesList.scss';
 
 export default function LatestArticlesList() {
-  return <div>LatestArticlesList</div>;
+  return (
+    <div className="latest-articles-list">
+      <div className="latest-articles-list__header">
+        <div className="latest-articles-list__title-wrapper">
+          <h2 className="latest-articles-list__title">Profuture Teknoloji Dergisi</h2>
+          <p className="latest-articles-list__subtitle">Güncel Yazılar</p>
+        </div>
+        <Button variant="outline" color="default" rightIcon="right">
+          Tümünü Görüntüle
+        </Button>
+      </div>
+    </div>
+  );
 }
