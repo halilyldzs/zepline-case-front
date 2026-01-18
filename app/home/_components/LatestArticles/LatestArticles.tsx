e0c5ae344dc33356382ff666da7fb0d7e457375f
halil <halilyldzs@gmail.com>
2026-01-18 14:42:15 +0300

    Add LatestArticlesList component and update LatestArticles layout; enhance SCSS for content wrapper styling.

--- app/home/_components/LatestArticles/LatestArticles.tsx
+++ app/home/_components/LatestArticles/LatestArticles.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import LatestJournalCard from './LatestJournalCard';
+import LatestArticlesList from './LatestArticlesList';
+import './LatestArticles.scss';
 
 export default function LatestArticles() {
   return (
     <section className="latest-articles container">
       <div className="latest-articles__content-wrapper">
         <LatestJournalCard />
+        <LatestArticlesList />
       </div>
     </section>
   );
---
+++ app/home/_components/LatestArticles/LatestArticlesList/LatestArticlesList.tsx
@@ -0,0 +1,5 @@
+import './LatestArticlesList.scss';
+
+export default function LatestArticlesList() {
+  return <div>LatestArticlesList</div>;
+}
